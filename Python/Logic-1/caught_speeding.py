494512fe6239e6d6e920c786e3a736289bf1ee13
Llambduh <llambduh@gmail.com>
2026-03-10 22:23:57 -0400

    Added answer to caught_speeding question

--- Python/Logic-1/caught_speeding.py
+++ Python/Logic-1/caught_speeding.py
@@ -1,2 +1,7 @@
 def caught_speeding(speed, is_birthday):
-    
+  birthday_bonus = 5 if is_birthday else 0
+  if speed <= 60 + birthday_bonus:
+      return 0
+  if speed <= 80 + birthday_bonus:
+      return 1
+  return 2
