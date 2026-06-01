2aa579eb72a68447de86d97a5d3a8eb967991f3e
Llambduh <llambduh@gmail.com>
2026-05-31 20:35:38 -0400

    Added round_sum answer

--- Python/Logic-2/round_sum.py
+++ Python/Logic-2/round_sum.py
@@ -1,2 +1,11 @@
-def round_sum(a, b, c):
-    
+def round_sum(a, b, c): 
+  def round10(num): 
+    # Get the rightmost digit
+    rightmost_digit = num % 10
+    # If rightmost digit is 5 or more, round up to the next multiple of 10
+    if rightmost_digit >= 5: 
+      return (num // 10 + 1) * 10 
+    # Otherwise, round down to the previous multiple of 10
+    else: 
+      return num // 10 * 10 
+  return round10(a) + round10(b) + round10(c)
